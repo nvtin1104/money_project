49aafd3fd33b746e72be3da238afa775df7711bf
Nguyễn Văn Tín <126067526+nvtin1104@users.noreply.github.com>
2024-04-27 16:58:01 +0700

    update

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,7 +1,7 @@
-import { WHITELIST_DOMAINS } from '~/utils/constants';
+import { WHITELIST_DOMAINS } from '~/utils/constants'
 // import { env } from '~/config/environment';
-import { StatusCodes } from 'http-status-codes';
-import ApiError from '~/utils/ApiError';
+import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 // Cấu hình CORS Option
 export const corsOptions = {
@@ -12,15 +12,15 @@ export const corsOptions = {
 
     // Kiểm tra dem origin có phải là domain được chấp nhận hay không
     if (WHITELIST_DOMAINS.includes(origin)) {
-      return callback(null, true);
+      return callback(null, true)
     }
 
     // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
-    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`));
+    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
   },
 
   optionsSuccessStatus: 200,
 
   // CORS sẽ cho phép nhận cookies từ request
-  credentials: true,
-};
+  credentials: true
+}
